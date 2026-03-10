48a3ff5360bbafcca483778bcec04b3602557afe
asispts <79239132+asispts@users.noreply.github.com>
2026-03-10 17:40:16 +0000

    Add vscode integration tests

---
+++ tests/extension/extension.test.ts
@@ -0,0 +1,25 @@
+import * as assert from "assert";
+import * as vscode from "vscode";
+
+suite("GitGraphPanel", () => {
+  suiteSetup(async () => {
+    const ext = vscode.extensions.getExtension("asispts.neo-git-graph");
+    await ext?.activate();
+  });
+
+  suiteTeardown(async () => {
+    await vscode.commands.executeCommand("workbench.action.closeAllEditors");
+  });
+
+  function isPanelOpen() {
+    return vscode.window.tabGroups.all
+      .flatMap((g) => g.tabs)
+      .some((t) => t.label === "(neo) Git Graph");
+  }
+
+  test("view command opens the panel", async () => {
+    await vscode.commands.executeCommand("neo-git-graph.view");
+    await new Promise((r) => setTimeout(r, 5));
+    assert.ok(isPanelOpen(), "Panel should be visible after executing view command");
+  });
+});
