0849de216f6bb84bc5e01e0a246b51840274c6b3
Asis Pattisahusiwa <79239132+asispts@users.noreply.github.com>
2026-03-26 06:27:28 +0700

    chore: add vscode settings (#21)

---
+++ esbuild.js
@@ -0,0 +1,64 @@
+const esbuild = require("esbuild");
+
+const production = process.argv.includes("--production");
+const watch = process.argv.includes("--watch");
+
+const esbuildProblemMatcherPlugin = {
+  name: "esbuild-problem-matcher",
+  setup(build) {
+    build.onStart(() => {
+      console.log("[watch] build started");
+    });
+    build.onEnd((result) => {
+      result.errors.forEach(({ text, location }) => {
+        console.error(`✘ [ERROR] ${text}`);
+        console.error(`    ${location.file}:${location.line}:${location.column}:`);
+      });
+      console.log("[watch] build finished");
+    });
+  }
+};
+
+async function main() {
+  const extension = await esbuild.context({
+    entryPoints: ["src/extension.ts"],
+    bundle: true,
+    format: "cjs",
+    minify: production,
+    sourcemap: !production,
+    sourcesContent: false,
+    platform: "node",
+    target: "es6",
+    outfile: "out/extension.js",
+    external: ["vscode"],
+    logLevel: "silent",
+    plugins: [esbuildProblemMatcherPlugin]
+  });
+
+  const webview = await esbuild.context({
+    entryPoints: ["web/main.ts"],
+    bundle: true,
+    format: "iife",
+    minify: production,
+    sourcemap: !production,
+    sourcesContent: false,
+    target: "es6",
+    outfile: "out/web.min.js",
+    logLevel: "silent",
+    plugins: [esbuildProblemMatcherPlugin]
+  });
+
+  if (watch) {
+    await Promise.all([extension.watch(), webview.watch()]);
+  } else {
+    await extension.rebuild();
+    await extension.dispose();
+    await webview.rebuild();
+    await webview.dispose();
+  }
+}
+
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
